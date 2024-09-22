abe1c1fe95273b945d6ef3302a57b87312d2e857
haze272 <studiorain27@gmail.com>
2024-09-22 23:09:07 +0300

    feat(practice1): add algorhytms

--- practice1/main.py
+++ practice1/main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+# 1. Применить для тестовых вариантов и вариантов из репозиториев различные алгоритмы поиска ассоциативных правил при одинаковых начальных
+# условиях (при одинаковых пороговых значениях для поддержки и достоверности) и сравнить полученные результаты. Для тестовых вариантов выполнить
+# ручные расчеты (например, с применением MS Excel) и расчеты с применением
+# программных библиотек на языке Python. Для вариантов из репозиториев выполнить расчеты с применением программных библиотек на языке Python.
+# В качестве алгоритмов поиска ассоциативных правил использовать алгоритмы:
+#   Apriori (https://pypi.org/project/apriori-python/);
+#   Efficient Apriori (https://pypi.org/project/efficient-apriori/);
+#   FPGrowth (https://pypi.org/project/fpgrowth-py/).
+# 2. Сформировать базы ассоциативных правил с уровнем минимальной достоверности 60% и 80%. Вычислить для ассоциативных правил поддержку, достоверность, значимость.
+# 3. Оценить время формирования искомых ассоциативных правил с применением различных алгоритмов и построить диаграммы, позволяющие выполнить сравнительный анализ.
+# 4. Выполнить визуализацию ассоциативных правил (https://pypi.org/project/pyarmviz/).
+#
+# Dataset: csv where { Date, Time, Transaction, Item }
+import csv
+from apriori_python import apriori
+import pandas as pd
+from efficient_apriori import apriori as eff_apriori
+from fpgrowth_py import fpgrowth
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def data_generator(filepath):
+    def data_gen():
+        with open(filepath) as file:
+            for line in file:
+                yield tuple(k.strip() for k in line.split(','))
+
+    return data_gen
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    df = pd.read_csv("/home/berkunov/Documents/GitHub/ISaT_MIREA/practice1/BreadBasket_DMS.csv")
+    df = df.groupby('Transaction')['Item'].apply(list).tolist()
+
+    # apriori_python
+    # freq_item_set, rules = apriori(df, minSup=0.01, minConf=0.01)
+    # print(rules)
+
+    # efficient_apriori
+    # freq_item_set,  rules = eff_apriori(df, min_support=0.01, min_confidence=0.01)
+    # rules_rhs = filter(lambda rule: len(rule.lhs) == 1 and len(rule.rhs) == 1, rules)
+    # for rule in sorted(rules_rhs, key = lambda rule: rule.confidence):
+    #     print(rule)
+    # print(rules)
+
+    # fpgrowth
+    freq_item_set, rules = fpgrowth(df, minSupRatio=0.05, minConf=0.05)
+    print(rules)
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
