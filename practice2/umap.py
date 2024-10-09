7c67f3e61e20db971ac489607d1b93356f5cb307
haze272 <studiorain27@gmail.com>
2024-10-09 12:41:07 +0300

    feat(practice2): make umap working

--- practice2/umap.py
+++ practice2/umap.py
@@ -11,12 +11,13 @@ def main():
     sns.set(style='white', context='notebook', rc={'figure.figsize': (14, 10)})
 
     # Чтение данных из CSV
-    df = pd.read_csv("/home/berkunov/Documents/GitHub/ISaT_MIREA/practice2/mammoth.csv")
+    df = pd.read_csv("/home/berkunov/Documents/GitHub/ISaT_MIREA/practice2/mammoth2.csv")
 
     df = df.dropna()
     df.x.value_counts()
 
-    sns.pairplot(df.drop("x", axis=1), hue='z')
+    sns.pairplot(df.drop("x", axis=1), hue='y')
+    plt.show()
 
 
 if __name__ == '__main__':
